23db0d72eda3b388bd4a5c2a4f0c224c1cd927c8
ortiz <ortiz@ORTIZ-4>
2025-08-30 18:12:27 -0300

    Desenvolvimento de mecanismos de busca/Inserção/adição/remoção/modificação.

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,13 +1,28 @@
+using System.Collections.ObjectModel;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
+
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+    
 	public ListaProduto()
 	{
 		InitializeComponent();
-	}
 
-	private void ToolbarItem_Clicked(object sender, EventArgs e)
+		lst_produtos.ItemsSource = lista;
+    }
+
+    protected async override void OnAppearing()
+    {
+		List<Produto> tmp = await App._Db.GetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
 		try
 		{
@@ -19,4 +34,29 @@ public partial class ListaProduto : ContentPage
 			DisplayAlert("Ops", ex.Message, "OK");
 		}
 	}
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App._Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:c}";
+
+		DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
